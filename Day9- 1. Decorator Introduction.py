9c171d53b316f6fd8f707e26b856c45acd99676d
AtharProgrammer <160819493+AtharProgrammer@users.noreply.github.com>
2024-03-08 16:58:48 +0530

    Add files via upload

---
+++ Day9- 1. Decorator Introduction.py
@@ -0,0 +1,38 @@
+'''
+Decorators are a powerful and advanced feature in Python that allow you
+to modify or extend the behavior of functions or classes.
+They are widely used in frameworks like Flask, Django, and many others.
+
+1. Introduction to Decorators:
+In Python, functions are first-class objects, which means they can be
+passed around and manipulated just like any other object.
+Decorators are simply functions that wrap another function or method
+to modify its behavior.
+
+Function behavior refers to how a function behaves when it is called or invoked. This includes what the function does, how it
+interacts with its arguments, and what value (if any) it returns.
+
+Here are some aspects of function behavior:
+
+Input: Functions may accept zero or more input parameters (arguments)
+which provide the necessary data for the function to perform its task.
+
+Processing: Functions perform a specific task or set of tasks using the
+provided input. This can involve any number of operations such as calculations, data manipulation, or interactions
+with external resources.
+
+Output: Functions may produce output, which could be a return value,
+a side effect (such as modifying a mutable object), or simply printing
+information to the console.
+
+Side Effects: Functions may have side effects, which are changes to the
+program state beyond the return value. This could include modifying global
+variables, writing to files, or interacting with external systems.
+
+Error Handling: Functions may handle errors internally or propagate them
+to the caller by raising exceptions.
+
+Scope: Functions have access to variables in their
+own scope (local variables), as well as variables in
+outer scopes (enclosing function scopes and global scope).
+"""
